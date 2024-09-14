6712b03108e4c46ae435f4d3f67e78f7b7bee20d
heliodesouza <heliodesouza088@gmail.com>
2024-09-14 20:38:09 -0300

    documents

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,14 +1,23 @@
-import { Html, Head, Main, NextScript } from "next/document";
-import Document from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="pt">
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&family=Poppins:wght@400;500&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
+
