6596952c8d451f7ef25fa363804c4306bd559b33
heliodesouza <heliodesouza088@gmail.com>
2024-09-14 20:56:48 -0300

    footer2 e about

--- components/footer/index.jsx
+++ components/footer/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faWhatsapp, faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
-import styles from './Footer.module.scss';
+import styles from './footer.module.scss';
 
 const Footer = () => {
   return (
